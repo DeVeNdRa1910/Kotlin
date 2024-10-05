8edbe022d19e1d35635f4fef578f773913a089cf
Devendra <devendrav1910@gmail.com>
2024-10-05 18:14:34 +0530

    learn about pre increment and post increment

--- src/basic/operators.kt
+++ src/basic/operators.kt
@@ -1,2 +1,41 @@
 package basic
 
+fun main() {
+
+    var a = 34
+    var b = 12
+/*
+    //Arithmatic
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(b/a)
+    println(b%a)
+*/
+/*
+    //relational
+    println(a>b)
+    println(a<b)
+    println(a==b)
+    println(a>=b)
+    println(a<=b)
+*/
+    println("Initial value of a = $a")
+
+    // post increment
+    println("a++ = " + a++)
+    println(a)
+
+    // pre increment
+    println("++a = " + ++a)
+    println(a)
+
+    //post decrement
+    println("a-- = " + a--)
+    println(a)
+
+    // pre decrement
+    println("--a = " + --a)
+    println(a)
+
+}
