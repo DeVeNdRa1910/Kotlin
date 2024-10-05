a648fe90c9343f320a0a914cf392d85e1b0a4072
Devendra <devendrav1910@gmail.com>
2024-10-05 19:24:58 +0530

    impliment all three Loops

---
+++ src/Loops/doWhileLoop.kt
@@ -0,0 +1,10 @@
+package Loops
+
+fun main() {
+    //print 1 to 10
+
+    var i = 1;
+    do {
+        println(i++)
+    } while (i<=10)
+}
---
+++ src/Loops/forLoop.kt
@@ -0,0 +1,7 @@
+package Loops
+
+fun main() {
+    for (i in 1..10){
+        println(i)
+    }
+}
