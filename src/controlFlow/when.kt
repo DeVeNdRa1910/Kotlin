68900189743fbd1e6af2b9d3e3ca885d15143e2e
Devendra <devendrav1910@gmail.com>
2024-10-05 19:24:40 +0530

    impliment when in ranges of variable

--- src/controlFlow/when.kt
+++ src/controlFlow/when.kt
@@ -1,16 +1,18 @@
 package controlFlow
 
 fun main(){
-    var marks : Int = 1
+    var marks : Int = 72
     resultUsingWhen(marks)
 }
 
 fun resultUsingWhen(marks: Int) {
 
     when(marks) {
-        0 -> print("number is zero")
-        1 -> print("Number is one")
-        else -> print("Number is negative one")
+        in 0..49 -> print("Failed")
+        in 50..60  -> print("Third")
+        in 61..74 -> print("Second")
+        in 75..89 -> print("First")
+        else -> print("Excellent")
     }
     //just like switch case in java
     return
